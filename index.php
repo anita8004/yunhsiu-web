26ff897dcdd11692ae1213bad954c13ba3a94624
Yunhsiu <anita8004@gmail.com>
2017-01-18 23:55:49 +0800

    頁面調整
    新增project資料

--- index.php
+++ index.php
@@ -42,45 +42,49 @@
           <div class="item">
             <div class="pic">
               <div class="cover">
+                <a data-fancybox="gallery" href="styles/images/project/project1.jpg" class="icon icon-search"></a>
                 <a href="http://www.relaxed.com.tw/" class="icon icon-link" target="_blank"></a>
               </div>
-              <img src="styles/images/project-thumb/project1.jpg" alt="Yunhsiu">
+              <img src="styles/images/project/project1.jpg" alt="Yunhsiu">
             </div>
             <div class="text">旅樂客</div>
           </div>
           <div class="item">
             <div class="pic">
               <div class="cover">
-                <a href="http://www.gemcare.com.tw/" class="icon icon-link" target="_blank"></a>
-              </div>
-              <img src="styles/images/project-thumb/project3.jpg" alt="Yunhsiu">
-            </div>
-            <div class="text">人之初</div>
-          </div>
-          <div class="item">
-            <div class="pic">
-              <div class="cover">
-                <a href="http://www.asaiilerfan.com.tw/" class="icon icon-link" target="_blank"></a>
-              </div>
-              <img src="styles/images/project-thumb/project4.jpg" alt="Yunhsiu">
-            </div>
-            <div class="text">領航者</div>
-          </div>
-          <div class="item">
-            <div class="pic">
-              <div class="cover">
                 <a data-fancybox="gallery" href="styles/images/project/project2.jpg" class="icon icon-search"></a>
+                <a href="http://www.timelessbeauty.com.tw/" class="icon icon-link" target="_blank"></a>
               </div>
-              <img src="styles/images/project-thumb/project2.jpg" alt="Yunhsiu">
+              <img src="styles/images/project/project2.jpg" alt="Yunhsiu">
             </div>
             <div class="text">美無極</div>
           </div>
           <div class="item">
             <div class="pic">
               <div class="cover">
+                <a data-fancybox="gallery" href="styles/images/project/project3.jpg" class="icon icon-search"></a>
+                <a href="http://www.gemcare.com.tw/" class="icon icon-link" target="_blank"></a>
+              </div>
+              <img src="styles/images/project/project3.jpg" alt="Yunhsiu">
+            </div>
+            <div class="text">人之初</div>
+          </div>
+          <div class="item">
+            <div class="pic">
+              <div class="cover">
+                <a data-fancybox="gallery" href="styles/images/project/project4.jpg" class="icon icon-search"></a>
+                <a href="http://www.asaiilerfan.com.tw/" class="icon icon-link" target="_blank"></a>
+              </div>
+              <img src="styles/images/project/project4.jpg" alt="Yunhsiu">
+            </div>
+            <div class="text">領航者</div>
+          </div>
+          <div class="item">
+            <div class="pic">
+              <div class="cover">
                 <a data-fancybox="gallery" href="styles/images/project/project5.jpg" class="icon icon-search"></a>
               </div>
-              <img src="styles/images/project-thumb/project5.jpg" alt="Yunhsiu">
+              <img src="styles/images/project/project5.jpg" alt="Yunhsiu">
             </div>
             <div class="text">高創</div>
           </div>
@@ -89,9 +93,38 @@
               <div class="cover">
                 <a data-fancybox="gallery" href="styles/images/project/project6.jpg" class="icon icon-search"></a>
               </div>
-              <img src="styles/images/project-thumb/project6.jpg" alt="Yunhsiu">
+              <img src="styles/images/project/project6.jpg" alt="Yunhsiu">
             </div>
             <div class="text">金龍俊</div>
+          </div>
+          <div class="item">
+            <div class="pic">
+              <div class="cover">
+                <a data-fancybox="gallery" href="styles/images/project/project7.png" class="icon icon-search"></a>
+                <a href="http://tw.8way.com.tw/" class="icon icon-link" target="_blank"></a>
+              </div>
+              <img src="styles/images/project/project7.png" alt="Yunhsiu">
+            </div>
+            <div class="text">八方雲集</div>
+          </div>
+          <div class="item">
+            <div class="pic">
+              <div class="cover">
+                <a data-fancybox="gallery" href="styles/images/project/project8.png" class="icon icon-search"></a>
+                <a href="http://www.tcygroup.com/index.php" class="icon icon-link" target="_blank"></a>
+              </div>
+              <img src="styles/images/project/project8.png" alt="Yunhsiu">
+            </div>
+            <div class="text">采妍</div>
+          </div>
+          <div class="item">
+            <div class="pic">
+              <div class="cover">
+                <a data-fancybox="gallery" href="styles/images/project/project9.png" class="icon icon-search"></a>
+              </div>
+              <img src="styles/images/project/project9.png" alt="Yunhsiu">
+            </div>
+            <div class="text">正隆形象頁</div>
           </div>
         </div>
       </div>
